167a6b8326303c4fa7568b894cf58558470ad490
Stefan Kaes <stefan.kaes@xing.com>
2021-12-26 18:01:28 +0100

    updated to ruby 3.1.0

--- build_ruby.rb
+++ build_ruby.rb
@@ -12,10 +12,10 @@ iteration i
 
 vendor "skaes@railsexpress.de"
 
-ruby_version = "3.0.3"
-patchlevel = 157
+ruby_version = "3.1.0"
+patchlevel = 0
 source "https://railsexpress.de/downloads/ruby-#{ruby_version}-p#{patchlevel}.tar.gz",
-       checksum: '43cd667f81033b43cfc3de627908d461dc52b7f2c94e1f6f3f686575235381d1'
+       checksum: 'a0583e919d1bddc0448b2f908c252c5636801da436ea61f1b9436d4a9e12e1f6'
 
 build_depends "autoconf"
 build_depends "automake"
@@ -80,7 +80,7 @@ run "make", "install"
 run "cd", ".."
 run "mkdir", "-p", "#{prefix}/etc"
 run "cp", ".gemrc", "#{prefix}/etc/gemrc"
-run "#{prefix}/bin/gem", "update", "-q", "--system", "3.2.33"
+# run "#{prefix}/bin/gem", "update", "-q", "--system", "3.3.3"
 
 plugin "exclude"
 exclude "/root/**"
